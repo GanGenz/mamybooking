6daba7cdf7cef220dfed2f9c673b052fc26415a3
GanGenz <jquery4me@gmail.com>
2017-02-05 17:50:46 +0700

    update amphurs database
    update amphurs database

--- database/migrations/2017_02_04_152236_create_amphurs_table.php
+++ database/migrations/2017_02_04_152236_create_amphurs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCarAmphursTable extends Migration
+class CreateAmphursTable extends Migration
 {
     /**
      * Run the migrations.
